afb4c736d9d6da8454d17d0a871a453578b6835b
Danillo Medeiros <danillo1ufpb@hotmail.com>
2020-09-08 16:32:10 -0300

    projeto criado

---
+++ src/main/java/com/curso/cursoudm/CursoudmApplication.java
@@ -0,0 +1,13 @@
+package com.curso.cursoudm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoudmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoudmApplication.class, args);
+	}
+
+}
